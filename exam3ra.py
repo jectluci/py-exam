b512bc31070596abc2cfa9c08283e32cb7e007ec
jectluci <esteafnoject@gmail.com>
2022-03-07 01:52:22 -0500

    nuevos archivos: Pipfile 	nuevos archivos: Pipfile.lock 	nuevos archivos: exam3ra.py

---
+++ exam3ra.py
@@ -0,0 +1,21 @@
+from functools import reduce
+
+
+def proceso_Calculo(n):
+    numero = 0
+    suma = 0
+    lista = []
+    lista_1 = []
+
+    for i in range(1, n):
+        lista.append(i)
+        print(i)
+
+
+
+
+
+if __name__ == "__main__":
+    n = int(input("insertar numero: "))
+
+    proceso_Calculo(n)
